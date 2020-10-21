00d11385ebfd19c992d3a13f2625dc6a7e8ca142
halawimaher <halawimaher@gmail.com>
2020-10-21 15:08:12 +0300

    step 4

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/test', function(req, res) {
-    res.send({status:200, message:"ok"})
+    res.status(200).send("ok")
 });
 
 app.get('/time', function(req, res) {
-    res.send({status:200, message: TIME})
+    res.status(200).send(TIME)
+});
+
+app.get('/hello', function(req, res) {
+    res.status(200).send("Hello")
+});
+
+app.get('/hello/:id', function(req, res) {
+    res.status(200).send("Hello " + req.params.id)
+});
+
+app.get('/search', function(req, res) {
+    if(req.query.s == "" || req.query.s == undefined){res.status(500).send('You have to enter a search')}
+    else{res.status(200).send("OK! You searched for:" + req.query.s)}
 });
